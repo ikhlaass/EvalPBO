213729e57b49d66001963d18432bd67ad81f23d3
LAB CV-21 <andiikhlas107@gmail.com>
2025-03-13 14:15:17 +0800

    update

--- 251_ikhlas/src/Java.java
+++
@@ -1,9 +0,0 @@
-
-public class Java {
-    public static void main(String[] args) {
-        Mahasiswa yy = new Mahasiswa();
-        yy.tampil();
-        
-    }
-    
-}
--- 251_ikhlas/src/nilai.java
+++
@@ -1,8 +0,0 @@
-
-public class nilai {
-String nama = "Andi Ikhlas";
-String nim = "13020230251";
-String kelas = "A7";
-int nilai[] = {100, 70, 98};
-String Matkul[] = {"PBO", "RPL", "OOP"};
-}
